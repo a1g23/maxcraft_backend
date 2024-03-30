1116a794c28eed641007e2e9b5250b1ab3e65b0f
Alex Greenberg <acgreenberg2@gmail.com>
2024-03-30 12:50:43 -0400

    updated viewsets and urls

--- products/views.py
+++ products/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from .models import Product
+from rest_framework import viewsets, permissions
+from .serializers import ProductSerializer
 
-# Create your views here.
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset=Product.objects.all()
+    serializer_class=ProductSerializer
+    permission_classes=[permissions.AllowAny]
